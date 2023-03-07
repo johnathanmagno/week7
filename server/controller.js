7a1e658624c4801deeff2173fedeb2af3a7b4542
jmagno <j@magno.io>
2023-03-07 00:31:48 -0600

    part 1

---
+++ server/controller.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["Advice is like kissing. It costs nothing and is a pleasant thing to do.","All your hard work will pay off","You'll meet a man at Denny's and he'll be angry","You'll be able to run fast","We've been trying to reach out about your cars extended warranty."];
+      
+        // choose random fortune
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
+    }
+
+}
